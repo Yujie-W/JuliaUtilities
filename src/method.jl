a93c6192d007509bccffda0c6d9e6c704ff61d90
Yujie <jesiner@gmail.com>
2021-05-04 15:53:50 -0700

    neld-mead method with history

--- src/method.jl
+++ src/method.jl
@@ -56,19 +56,23 @@ Base.@kwdef mutable struct NelderMeadMethod{FT<:AbstractFloat} <:
     "Number of parameters to optimize"
     N::Int = 2
     "Initial values"
-    x_inis::Array{FT,1} = zeros(FT,N+1)
-    "Simplex array of array with dimension (N+1) * (N+1)"
-    simplex::Array{Array{FT,1},1} = [zeros(FT,N+1) for i in 1:(N+1)]
+    x_inis::Vector{FT} = zeros(FT,N+1)
+    "Simplex vector of vector with dimension (N+1) * (N+1)"
+    simplex::Vector{Vector{FT}} = [zeros(FT,N+1) for i in 1:(N+1)]
 
     # temporary containers
     "Centroid"
-    cen_x::Array{FT,1} = deepcopy(x_inis)
+    cen_x::Vector{FT} = deepcopy(x_inis)
     "Reflection"
-    ref_x::Array{FT,1} = deepcopy(x_inis)
+    ref_x::Vector{FT} = deepcopy(x_inis)
     "Expansion"
-    exp_x::Array{FT,1} = deepcopy(x_inis)
+    exp_x::Vector{FT} = deepcopy(x_inis)
     "Contraction"
-    con_x::Array{FT,1} = deepcopy(x_inis)
+    con_x::Vector{FT} = deepcopy(x_inis)
+
+    # history Vector
+    "history of all simulations"
+    history::Vector{Vector{FT}} = Vector{FT}[]
 end
 
 
@@ -170,19 +174,19 @@ $(TYPEDFIELDS)
 Base.@kwdef mutable struct ReduceStepMethodND{FT<:AbstractFloat} <:
                            AbstractCRSMethod{FT}
     "Lower bound"
-    x_mins::Array{FT,1} = zeros(FT,2)
+    x_mins::Vector{FT} = zeros(FT,2)
     "Upper bound"
-    x_maxs::Array{FT,1} =  ones(FT,2)
+    x_maxs::Vector{FT} = ones(FT,2)
     "Initial guess"
-    x_inis::Array{FT,1} = FT[0.5, 0.5]
+    x_inis::Vector{FT} = FT[0.5, 0.5]
     "Target x"
-    x_targ::Array{FT,1} = deepcopy(x_inis)
+    x_targ::Vector{FT} = deepcopy(x_inis)
     "Temporary x"
-    x_temp::Array{FT,1} = deepcopy(x_inis)
+    x_temp::Vector{FT} = deepcopy(x_inis)
     "Initial step"
-    Δ_inis::Array{FT,1} = FT[0.1, 0.1]
+    Δ_inis::Vector{FT} = FT[0.1, 0.1]
     "Operation step"
-    Δ_oper::Array{FT,1} = deepcopy(Δ_inis)
-    "Array of judges"
-    Δjd::Array{Bool,1} = [false for i in 1:length(x_inis)]
+    Δ_oper::Vector{FT} = deepcopy(Δ_inis)
+    "Vector of judges"
+    Δjd::Vector{Bool} = [false for i in 1:length(x_inis)]
 end
