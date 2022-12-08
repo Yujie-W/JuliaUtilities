acc1d493af7e7674c66975dadb789f7399b69c90
Yujie Wang <jesiner@gmail.com>
2022-12-08 15:58:54 -0800

    move line regress

--- src/PlotPlants.jl
+++ src/PlotPlants.jl
@@ -16,23 +16,15 @@ using ..NetcdfIO: read_nc
 include("../packages/PlotPlants.jl/src/canvas.jl"    )
 include("../packages/PlotPlants.jl/src/decoration.jl")
 include("../packages/PlotPlants.jl/src/latex.jl"     )
+include("../packages/PlotPlants.jl/src/scientific.jl")
 
 
 include("../packages/PlotPlants.jl/src/plots/density.jl"     )
 include("../packages/PlotPlants.jl/src/plots/ellipse.jl"     )
 include("../packages/PlotPlants.jl/src/plots/hexbin.jl"      )
-include("../packages/PlotPlants.jl/src/plots/line_regress.jl")
 include("../packages/PlotPlants.jl/src/plots/stoma.jl"       )
 
 include("../packages/PlotPlants.jl/src/statistics/density.jl")
-
-include("../packages/PlotPlants.jl/src/string/latex_symbol.jl")
-include("../packages/PlotPlants.jl/src/string/latex_unit.jl"  )
-include("../packages/PlotPlants.jl/src/string/parse_symbol.jl")
-include("../packages/PlotPlants.jl/src/string/sub_super.jl"   )
-
-include("../packages/PlotPlants.jl/src/text/font.jl"  )
-include("../packages/PlotPlants.jl/src/text/render.jl")
 
 
 end # module
